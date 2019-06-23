173182d50b2b998d1729992f1dce469e90478638
Valeri Karpov <val@karpov.io>
2019-06-23 12:48:11 -0700

    chore: add opengraph tags

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,11 @@ module.exports = params => `
   <head>
     <title>${params.title} - Mastering JS</title>
 
+    <meta property="og:title" content="${params.title}" />
+    <meta property="og:site_name" content="Mastering JS" />
+    <meta property="og:description" content="${params.description}" />
+    <meta property="og:type" content="article" />
+
     <link rel="stylesheet" href="/assets/style.css" />
     <link rel="stylesheet" href="/assets/github.css" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Inconsolata:400,700" rel="stylesheet">
