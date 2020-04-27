cd6422dc6eb905af0a61af1434cf4e11e8c69244
Valeri Karpov <val@karpov.io>
2020-04-27 16:04:15 -0400

    add css loader tutorial

--- examples/example/app.js
+++ examples/example/app.js
@@ -1,1 +1,6 @@
-module.exports = 'Hello, World';
+function myApp() {
+        require('./style.css');
+
+        document.querySelector('#content').innerHTML = '<h1>Hello, World</h1>';
+      }
+myApp();
