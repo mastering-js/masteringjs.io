4f03a57e73a4ba3570572da7a2fafbd6c03e669f
Valeri Karpov <val@karpov.io>
2020-09-15 12:28:35 -0400

    add jsjabber link

--- index.js
+++ index.js
@@ -1912,6 +1912,18 @@ async function run() {
       `,
       price: '<strike>$39.99</strike> <b>$35.99</b>'
     }));
+  fs.writeFileSync('./ebooks/mastering-mongoose-javascript-jabber.html',
+    require('./components/ebooks/mongoose')({
+      paypalButton: `
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="JAZZNXHVDPM52">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+      `,
+      price: '<strike>$39.99</strike> <b>$35.99</b>'
+    }));
   
   fs.writeFileSync('./request-invite.html', layout({
     title: 'Request Invite',
