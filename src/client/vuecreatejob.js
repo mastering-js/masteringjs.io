2eb85598a8fda965887098a7c5a73680a67d43d4
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2021-04-26 16:56:14 -0400

    added more fields

--- src/client/vuecreatejob.js
+++ src/client/vuecreatejob.js
@@ -7,16 +7,49 @@ Vue.createApp({
     company: '',
     logo: null,
     title: '',
-    location: 'Anywhere'
+    location: 'Anywhere',
+    sticky: false,
+    description: null,
+    url: null,
+    instructions: null,
+    email: null,
+    feedback: null,
+    invoiceAddress: null,
+    invoiceNotes: null,
+    tags: [],
+    tool: null,
   }),
   methods: {
     async postJob() {
+      const formData = new FormData();
+      formData.append('logo', this.logo);
+      const headers = {'Content-Type': 'multipart/form-data'};
       await axios.post(server + '/api/createjob', {
         company: this.company,
         title: this.title,
-        location: this.location
-      });
+        location: this.location,
+        email: this.email,
+        tags: this.tags,
+        sticky: this.sticky,
+        description: this.description,
+        url: this.url,
+        instructions: this.instructions,
+        feedback: this.feedback,
+        invoiceAddress: this.invoiceAddress,
+        invoiceNotes: this.invoiceNotes,
+        logo: formData
+      }, {headers});
       console.log('Done');
+    },
+    addTool() {
+      this.tags.push(this.tool);
+      this.tool = null;
+    },
+    removeTool() {
+      this.tags.pop();
+    },
+    assignImage() {
+      this.logo = this.$refs.file.files[0];
     }
   },
   template: `
@@ -34,6 +67,51 @@ Vue.createApp({
         <div>
           <label>Location</label>
           <input type="text" v-model="location" />
+        </div>
+        <div>
+          <label>Sticky?</label>
+          <input type="checkbox" v-model="sticky" />
+        </div>
+        <div>
+          <div><label>Description</label></div>
+          <textarea v-model="description">Enter Text Here</textarea>
+        </div>
+          <form action="" @submit.prevent="addTool()">
+            <div v-for="tag in tags" :key="tag">{{tag}}</div>
+              <label>Add Framework</label>
+              <input type = "text" v-model="tool"/>
+              <div>
+              <button type = "submit">Submit</button>
+              <button @click="removeTool()">Remove most recent entry</button>
+              </div>
+          </form>
+        <div>
+          <div><label>Company Image</label></div>
+          <input type="file" @change="assignImage" ref = "file"/>
+        </div>
+        <div>
+          <label>Apply URL</label>
+          <input type="url" v-model="url"/>
+        </div>
+        <div>
+          <div><label>How To Apply</label></div>
+          <textarea v-model="instructions">To Apply</textarea>
+        </div>
+        <div>
+          <label>Company Email</label>
+          <input type="email" v-model="email"/>
+        </div>
+        <div>
+          <div><label>Feedback</label></div>
+          <textarea v-model="feedback">Type here</textarea>
+        </div>
+        <div>
+          <label>Invoice Address</label>
+          <input type="text" v-model="invoiceAddress" />
+        </div>
+        <div>
+          <label>Invoice Notes</label>
+          <input type="text" v-model="invoiceNotes" />
         </div>
         <button type="submit">Submit</button>
       </form>
