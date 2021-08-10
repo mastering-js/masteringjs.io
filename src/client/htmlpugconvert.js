68ae605f7362b1f8333c56399804b4cb6a0cf138
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2021-08-10 15:30:19 -0400

    removed comments

--- src/client/htmlpugconvert.js
+++ src/client/htmlpugconvert.js
@@ -4,12 +4,7 @@ window.pug = pug;
 window.html = html;
 
 window.convert = function convert() {
-    console.log('hello')
-// const html2pug = require('html2pug');
 const html2jade = require('html2jade');
-// const pug = document.querySelector('#pug')
-//const html = document.querySelector('#html')
-// pug.setValue(html2pug(html.getValue(), { tabs: true }));
 html2jade.convertHtml(window.html.getValue(), {}, function(err, jade) {
     window.pug.setValue(jade);
 })
