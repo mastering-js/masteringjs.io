b2e7c4c3c87c544eec498c0970ff6dd0b3b98b36
Valeri Karpov <val@karpov.io>
2021-09-15 14:11:32 -0400

    ready for release

--- tutorials/htmltopug.js
+++ tutorials/htmltopug.js
@@ -1,1 +1,70 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t){const n=CodeMirror(document.querySelector("#pug"),{mode:"pug",lineNumbers:!0,readOnly:!0}),r=CodeMirror(document.querySelector("#html"),{mode:"xml",lineNumbers:!0});window.pug=n,window.html=r,window.convert=async function(){const e=await fetch("http://localhost:7071/api/htmltopug",{method:"POST",body:JSON.stringify({input:window.html.getValue(),fragment:document.getElementById("fragment").checked})}).then(e=>e.json());window.pug.setValue(e.result)},document.getElementById("work").addEventListener("click",convert)}]);
+! function(e) {
+  var t = {};
+
+  function n(r) {
+      if (t[r]) return t[r].exports;
+      var o = t[r] = {
+          i: r,
+          l: !1,
+          exports: {}
+      };
+      return e[r].call(o.exports, o, o.exports, n), o.l = !0, o.exports
+  }
+  n.m = e, n.c = t, n.d = function(e, t, r) {
+      n.o(e, t) || Object.defineProperty(e, t, {
+          enumerable: !0,
+          get: r
+      })
+  }, n.r = function(e) {
+      "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+          value: "Module"
+      }), Object.defineProperty(e, "__esModule", {
+          value: !0
+      })
+  }, n.t = function(e, t) {
+      if (1 & t && (e = n(e)), 8 & t) return e;
+      if (4 & t && "object" == typeof e && e && e.__esModule) return e;
+      var r = Object.create(null);
+      if (n.r(r), Object.defineProperty(r, "default", {
+              enumerable: !0,
+              value: e
+          }), 2 & t && "string" != typeof e)
+          for (var o in e) n.d(r, o, function(t) {
+              return e[t]
+          }.bind(null, o));
+      return r
+  }, n.n = function(e) {
+      var t = e && e.__esModule ? function() {
+          return e.default
+      } : function() {
+          return e
+      };
+      return n.d(t, "a", t), t
+  }, n.o = function(e, t) {
+      return Object.prototype.hasOwnProperty.call(e, t)
+  }, n.p = "", n(n.s = 0)
+}([function(e, t) {
+  var server = window.location.hostname === 'localhost' ?
+        'http://localhost:7071' :
+        'https://masteringjs-job-board.azurewebsites.net';
+
+  const n = CodeMirror(document.querySelector("#pug"), {
+          mode: "pug",
+          lineNumbers: !0,
+          readOnly: !0
+      }),
+      r = CodeMirror(document.querySelector("#html"), {
+          mode: "xml",
+          lineNumbers: !0
+      });
+  window.pug = n, window.html = r, window.convert = async function() {
+      const e = await fetch(server + "/api/htmltopug", {
+          method: "POST",
+          body: JSON.stringify({
+              input: window.html.getValue(),
+              fragment: true
+          })
+      }).then(e => e.json());
+      window.pug.setValue(e.result)
+  }, document.getElementById("work").addEventListener("click", convert)
+}]);
--- tutorials/node/html2pug.md
+++ tutorials/node/html2pug.md
@@ -1,23 +1,21 @@
-To convert your HTML files to Pug, you can use the [html2pug](https://www.npmjs.com/package/html2pug) and the [fs-extra](https://www.npmjs.com/package/fs-extra) npm packages.
+To convert your HTML files to Pug, you can use the [html2pug](https://www.npmjs.com/package/html2pug) npm package.
+The html2pug package currently only works in Node.js, so you need an API endpoint if you want to use it from a browser app.
+Below is an example of using html2pug in Node.js.
 
 ```javascript
-const fs = require('fs-extra');
 const html2pug = require('html2pug');
 
-async function test() {
-    const fileContents = await fs.readFile('test.html', 'utf-8');
-    const newFileContents = html2pug(fileContents, {tabs: false});
-    await fs.writeFile('test.html', newFileContents)
-    await fs.rename('test.html', 'test.html'.substr(0, 'test.html'.length - 5) + '.pug')
-}
-
-test();
+// h1 Hello, World
+html2pug('<h1>Hello, World</h1>', { tabs: false, fragment: true });
 ```
 
-**Note:** You could use node's fs module but `fs-extra` already uses `fs` and it has the benefit of being neater.
+The `fragment` option tells html2pug whether to wrap your HTML in an `<html>` tag, if it isn't already.
+`fragment` is `false` by default, which means html2pug will always prepend `html\n  head\n  body\n    ` to the output by default.
+
+Below is a live converter that uses `html2pug`.
 
 <div id='html'></div>
-<button id='work' onclick="window.convert">Turn HTML into Pug!</button> <input id='fragment' type='checkbox'/>Is this a fragment?
+<button id='work' onclick="window.convert">Turn HTML into Pug!</button>
 <div id='pug'></div>
 <script src="../../codemirror-5.62.2/lib/codemirror.js"></script>
 <link rel="stylesheet" href="../../codemirror-5.62.2/lib/codemirror.css">
