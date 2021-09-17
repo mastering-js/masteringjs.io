b403fc90b08e2b1cc294a195ed611427c40541cc
Valeri Karpov <val@karpov.io>
2021-09-17 18:07:51 -0400

    improve job stats

--- components/job-stats.js
+++ components/job-stats.js
@@ -12,6 +12,52 @@ module.exports = () =>
   <script src="https://unpkg.com/vue@2.x"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
   <script src="https://unpkg.com/vue-chartjs/dist/vue-chartjs.min.js"></script>
+
+  <style>
+    .row::after {
+      content: '';
+      clear: both;
+      display: table;
+    }
+
+    .row .col-4 {
+      width: 33.333333%;
+      float: left;
+    }
+
+    .stat {
+      padding: 10px;
+      margin: 10px;
+      border: 1px solid #ddd;
+      border-radius: 3px;
+      position: relative;
+    }
+
+    .stat .tooltip {
+      position: absolute;
+      display: none;
+      left: 0%;
+      top: 99%;
+      background-color: #ddd;
+      line-height: 1.25em;
+      padding: 10px;
+      border-radius: 3px;
+      width: calc(100% - 20px);
+    }
+
+    .stat:hover .tooltip {
+      display: block;
+    }
+
+    .stat-data {
+      font-size: 1.5em;
+      font-weight: bold;
+    }
+
+    canvas {
+      margin-top: 2em;
+    }
+  </style>
 </head>
 
 <body>
--- src/client/vuejobstats.js
+++ src/client/vuejobstats.js
@@ -1,12 +1,15 @@
 
 var server = window.location.hostname === 'localhost' ?
-  'http://localhost:7071' :
+'https://masteringjs-job-board.azurewebsites.net' : //'http://localhost:7071' :
   'https://masteringjs-job-board.azurewebsites.net';
+
+var search = new URLSearchParams(window.location.search.slice(1));
 
 // loads Jobs
 const app = new Vue({
   data() {
     return {
+      job: null,
       jobDetails: null,
       loading: true,
       error: false,
@@ -14,19 +17,52 @@ const app = new Vue({
   },
   template: `
   <div>
-  <div v-if="error">
-  Make sure you append the jobId as follows: masteringjs.io/jobs/detail#jobId
-  If the error still persists, contact Val@karpov.io
-  </div>
-  <div v-if="!loading">
-  <line-chart :jobDetails="jobDetails"></line-chart>
-  </div>
+    <div v-if="error">
+      Make sure you append the jobId as follows: masteringjs.io/jobs/detail#jobId
+      If the error still persists, contact val@karpov.io
+    </div>
+    <div v-if="!loading">
+      <h1>{{job.title}} @ {{job.company}}</h1>
+      <div class="row">
+        <div class="col-4">
+          <div class="stat">
+            <div class="stat-header">
+              Views (last 30 days)
+            </div>
+            <div class="tooltip">
+              Views are the number of times someone viewed the description of your job posting.
+              This means they clicked through on a Mastering JS job board ad to your job posting.
+            </div>
+            <div class="stat-data">
+              {{jobDetails.totalViews}}
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="stat">
+            <div class="stat-header">
+              Clicks (last 30 days)
+            </div>
+            <div class="tooltip">
+              Clicks are the number of times someone clicked "apply" on your job posting and was
+              redirected to the <a :href="job.url">link you provided to apply for the role</a>.
+            </div>
+            <div class="stat-data">
+              {{jobDetails.totalClicks}}
+            </div>
+          </div>
+        </div>
+      </div>
+      <line-chart :jobDetails="jobDetails"></line-chart>
+    </div>
   </div>
   `,
   async mounted() {
-    if (window.location.hash != null && window.location.hash.length > 1) {
-      const jobId = window.location.hash.replace(/^#/, '');
-      this.jobDetails = await axios.get(server+'/api/jobStats/'+jobId).then((res) => {return res.data.information});
+    if (search.has('jobId')) {
+      const jobId = search.get('jobId');
+      const res = await axios.get(server+'/api/jobStats/'+jobId);
+      this.jobDetails = res.data.information;
+      this.job = res.data.job;
       return this.loading = false;
     }
     return this.error = true;
@@ -40,17 +76,21 @@ Vue.component('line-chart', {
     this.renderChart({
       labels: this.jobDetails.numClicks.map(element => element.day),
       datasets: [{
-        label: 'Number of Clicks a Day',
+        label: 'Clicks/Day',
         backgroundColor: '#f87979',
         fill: false,
         data: this.jobDetails.numClicks.map(element => element.applyClicks)
       }, {
-        label: 'Number of Views a Day',
+        label: 'Views/Day',
         fill: false,
         backgroundColor: '#4dc9f6',
         data: this.jobDetails.numViews.map(element => element.views)
       }],
-    }, {responsive: true, maintainAspectRatio: true, scales: { xAxes: [{ ticks: { autoSkip: false } }] }, scaleShowValues: true})
+    }, {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: { xAxes: [{ ticks: { autoSkip: false } }] }, scaleShowValues: true
+    });
   }
 })
 app.$mount('#content');
