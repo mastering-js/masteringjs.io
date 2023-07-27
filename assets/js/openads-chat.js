d3b2385c1fe59885d773bead61997d0307e04670
Valeri Karpov <val@karpov.io>
2023-07-27 13:06:01 -0400

    basic chatbot ui

--- assets/js/openads-chat.js
+++ assets/js/openads-chat.js
@@ -14,5 +14,34 @@
   const chatExit = document.querySelector('.openads-chat-exit');
   chatExit.onclick = function() {
     chatWindow.classList.remove('show');
-  }
+  };
+
+  const newMessageInput = document.querySelector('.openads-chat-input input');
+  const newMessageButton = document.querySelector('.openads-chat-submit');
+  const chatHistory = document.querySelector('.openads-chat-history');
+
+  window.submitOpenAdsMessage = function submitOpenAdsMessage() {
+    const value = newMessageInput.value;
+    newMessageButton.disabled = true;
+    newMessageInput.disabled = true;
+
+    const newMessage = document.createElement('div');
+    newMessage.classList.add('openads-chat-history-message');
+    newMessage.innerHTML = `
+    <div class="participant">
+      <img src="/assets/images/guest.svg">
+      Guest
+    </div>
+    <div class="openads-chat-message-body">
+      ${value}
+    </div>
+    `;
+    chatHistory.appendChild(newMessage);
+
+    setTimeout(() => {
+      newMessageInput.value = '';
+      newMessageInput.disabled = false;
+      newMessageButton.disabled = false;
+    }, 2000);
+  };
 })();
