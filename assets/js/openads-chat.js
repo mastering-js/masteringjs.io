2e6cc5143632b402a61ce711a2a5f9a5ee9193ee
Valeri Karpov <val@karpov.io>
2023-07-29 15:14:21 -0400

    quick improvements for openads chat

--- assets/js/openads-chat.js
+++ assets/js/openads-chat.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
 
+  const endpoint = 'https://mj-chatbot-production.up.railway.app/api/chat';
+
   const widget = document.createElement('div');
   widget.innerText = 'Ask me anything about JavaScript';
   widget.classList.add('openads-widget');
@@ -20,8 +22,11 @@
   const newMessageButton = document.querySelector('.openads-chat-submit');
   const chatHistory = document.querySelector('.openads-chat-history');
 
-  window.submitOpenAdsMessage = function submitOpenAdsMessage() {
+  window.submitOpenAdsMessage = async function submitOpenAdsMessage() {
     const value = newMessageInput.value;
+    if (!value) {
+      return;
+    }
     newMessageButton.disabled = true;
     newMessageInput.disabled = true;
 
@@ -38,10 +43,35 @@
     `;
     chatHistory.appendChild(newMessage);
 
-    setTimeout(() => {
-      newMessageInput.value = '';
-      newMessageInput.disabled = false;
-      newMessageButton.disabled = false;
-    }, 2000);
+    let response;
+    let error = false;
+    try {
+      response = await fetch('https://mj-chatbot-production.up.railway.app/api/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ question: value })
+      }).then(res => res.json()).then(res => res.response);
+    } catch (err) {
+
+    }
+
+    newMessageInput.value = '';
+    newMessageInput.disabled = false;
+    newMessageButton.disabled = false;
+
+    const newResponse = document.createElement('div');
+    newResponse.classList.add('openads-chat-history-message');
+    newResponse.innerHTML = `
+    <div class="participant">
+      <img src="/assets/logo.svg">
+      Mastering JS
+    </div>
+    <div class="openads-chat-message-body">
+      ${response}
+    </div>
+    `;
+    chatHistory.appendChild(newResponse);
   };
 })();
